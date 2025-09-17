893a7298d30fc8b26da04dba6cee72fbcaddf14c
Guilherme Henrique dos Santos <gui.santos14562@gmail.com>
2025-09-16 21:25:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('botao')
+const botao2 = document.getElementById('botao2')
+
+
+botao.addEventListener('click', () =>{
+    document.body.style.backgroundColor = 'cyan'
+})
+
+botao2.addEventListener('click', () =>{
+    document.body.style.backgroundColor = '#808080'
+})
